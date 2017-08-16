ae7c86f0abc4fae44544e3792f723a82ff28a637
dikshit2848 <dikshitsharma2848@gmail.com>
2017-08-16 20:18:15 +0530

    First project of Android basics NanoDegree

---
+++ Single Screen app/Project1/app/src/main/java/com/example/diksh/project1/Main.java
@@ -0,0 +1,13 @@
+package com.example.diksh.project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
